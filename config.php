5f2b1b5f1accb81dabe498b2f25426467a7e7767
ROIHEE <thanawat58311@gmail.com>
2024-09-19 01:38:15 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://division.dwr.go.th/asia/',
+        'url'		=> 'https://agri.kps.ku.ac.th/education/FundFile/asia/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
